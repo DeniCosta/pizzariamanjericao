3dd65e16a4c1c7b1454727a1c41b146ce03517eb
isabellalcvicente <isabellavicente@ufpr.br>
2023-06-03 10:33:51 -0300

    Alterações no html do form

---
+++ JavaScript/scriptFormulario.js
@@ -0,0 +1,16 @@
+lenght = 11 caracteres 
+var nome = null
+var telefone = telefone.lenght
+var mensagem = null
+
+if (nome != null){
+    console.log("Vá para o próximo campo")
+}
+
+if (telefone === ){
+    console.log("Vá para o próximo campo")
+}
+
+if (mensagem != null){
+    console.log("Vá para o próximo campo")
+}
